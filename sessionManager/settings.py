974f87e0e7937e33ea7f14fc2f7660dca6995504
Saeid Aghashahi <imsintech@icloud.com>
2022-08-22 03:07:47 +0430

    set allowed hosts to *

--- sessionManager/settings.py
+++ sessionManager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5vl0_=)6agfrmik$d&h%9vlx0bj4$&9z&77kwq1%*s%kbfmayt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
