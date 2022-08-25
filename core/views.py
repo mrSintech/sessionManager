6d282baa943af5f5e66b1e87bed0204bc917af52
Saeid Aghashahi <imsintech@icloud.com>
2022-08-25 18:34:54 +0430

    TEST

--- core/views.py
+++ core/views.py
@@ -72,6 +72,7 @@ class RoomViewSet(viewsets.ViewSet):
                 start = session['start']
                 end   = session['end']
         
+        start = datetime.strptime(start)
         tz    = pytz.timezone('Asia/Kolkata')
         start = start.astimezone(tz=timezone.utc)
         messages.append(start)
