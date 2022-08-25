0fface6c89e757876037801e5af8ad7efbb70181
Saeid Aghashahi <imsintech@icloud.com>
2022-08-25 17:58:16 +0430

    TEST

--- core/views.py
+++ core/views.py
@@ -62,7 +62,7 @@ class RoomViewSet(viewsets.ViewSet):
         
         sessions = json.loads(sessions)
         for session in sessions:
-            messages.append(session)
+            messages.append(session['title'])
         
         res = tools.response_prepare(messages, True, None)
         return Response(res)
