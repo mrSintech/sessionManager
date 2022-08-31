b6f558f6b9a36443ce7f8e1b4f41873fcfe3aa2b
Saeid Aghashahi <imsintech@icloud.com>
2022-08-31 15:40:55 +0430

    TEST: cicd

--- sessionManager/settings.py
+++ sessionManager/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         'NAME': 'session_manager_core',
         'USER': 'web_api',
         'PASSWORD': 'safepass',
-        'HOST': '188.121.121.112',
+        'HOST': 'localhost',
         'PORT': '5432',
     }
 }
